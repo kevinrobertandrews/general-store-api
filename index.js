f85d5cddb561dd313ae3d980a0c8a0a75a1f6a50
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2025-03-27 11:29:45 -0400

    remove Dockerfile
    Let's try going with Netlify.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.status(200).send("OKAY");
+  res.status(200).json({ status: "success", message: "Ok." });
 });
 
 app.listen(3000, () => {
