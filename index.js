e2ddc477cc8ee069f7b6447f3aa3fa983d86d189
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2025-03-27 15:31:42 -0400

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({ origin: ["testing-20250327"] }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
