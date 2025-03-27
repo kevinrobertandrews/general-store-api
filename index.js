ef610e7a86b9e3ba259dc4ac74dbb881eeacaa8b
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2025-03-27 12:58:07 -0400

    change port

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
   res.status(200).json({ status: "success", message: "Ok." });
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("listening...");
 });
